59d13e00dcb7bea15914ff2252ec776851ce1db9
Satwik2529 <satwik2529@gmail.com>
2025-03-18 10:21:27 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@
 import { Dashboard } from './pages/dashboard'
 import { Signin } from './pages/Signin'
 import { Signup } from './pages/Signup'
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import { BrowserRouter,Navigate,Route,Routes } from 'react-router-dom'
 
 function App() {
    return <BrowserRouter>
    <Routes>
+   <Route path="/" element={<Navigate to="/signup" />} />  
    <Route path="/signup" element={<Signup></Signup>}></Route>
    <Route path="/signin" element={<Signin></Signin>}></Route>
    <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
